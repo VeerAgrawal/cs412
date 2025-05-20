4d6b0bb655f8044f4c4326e3c079b791d417ae6b
VeerAgrawal <>
2025-05-20 07:27:13 -0400

    updated settings files error

--- cs412/settings.py
+++ cs412/settings.py
@@ -136,5 +136,5 @@ import socket
 CS_DEPLOYMENT_HOSTNAME = 'cs-webapps.bu.edu'
 
 if socket.gethostname() == CS_DEPLOYMENT_HOSTNAME:
-    STATIC_URL = '/veeragrawal/static/'
-    MEDIA_URL = '/veeragrawal/media/'
+    STATIC_URL = '/veer1/static/'
+    MEDIA_URL = '/veer1/media/'
